4a59a5ddec5c39c6c8e0b32b43ec1d4eff4084da
Mark Pedrotti <pedrottimark@gmail.com>
2018-03-03 16:48:33 -0500

    Correct third text property in receivedString

--- 2018-03-06/index.js
+++ 2018-03-06/index.js
@@ -5,7 +5,7 @@
     '"todos":[',
     '{"completed":true,"text":"Render <strong>markup</strong> from data"},',
     '{"completed":false,"text":"Add <strong>interactive</strong> behavior to rendered markup"},',
-    '{"completed":false,"text":"Call <code>querySelector</code> method to find an element"}',
+    '{"completed":false,"text":"Call <code>find</code> method with selector string"}',
     ']',
     '}',
   ].join('');
